d9a620a44e4206d94fb811e7fccef5584e6d2698
cornerguy <66196654+cornerguy@users.noreply.github.com>
2026-07-18 06:48:09 +0530

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -27,7 +27,7 @@ export async function refreshOrigins() {
 }
 app.use(cors({
     origin(origin, callback) {
-        console.loh(allowedOrigins);
+        console.log(allowedOrigins);
         if (!origin)
             return callback(null, true);
         if (allowedOrigins.has(origin) || STATIC_ORIGINS.has(origin)) {
