625262120469befd9ee97e021bad8f707260a761
cornerguy <66196654+cornerguy@users.noreply.github.com>
2026-08-19 21:52:58 +0530

    Update index.js

--- dist/utils/storage/index.js
+++ dist/utils/storage/index.js
@@ -1,5 +1,5 @@
 import { LocalStorageAdapter } from "./local.adapter.js";
 import { STORAGE_TYPE } from "../../env.js";
 import { S3StorageAdapter } from "./cloud.adapter.js";
-console.log(STORAGE_TYPE);
+
 export const storage = STORAGE_TYPE == "s3" ? new LocalStorageAdapter() : new S3StorageAdapter();
